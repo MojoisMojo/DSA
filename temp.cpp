8493075c1d61a2b4cf8c2eed27312710065cb738
MojoisMe <2377862945@qq.com>
2023-10-12 21:32:47 +0800

    .

--- temp.cpp
+++ temp.cpp
@@ -3,14 +3,14 @@
 #include <string>
 #include <stack>
 #include <cmath>
-
+#include <bits/stdc++.h>
 using namespace std;
 
 int main() {
     int idx[3] = { 1,2,3 };
     int *pos1 = idx,*pos2 = idx;
-    cout << *(pos1++)**(pos2++);
-
+    cout << *(pos1++) * *(pos2++);
+    string s = "" + 1;
     //system("pause");
     return 0;
 }
