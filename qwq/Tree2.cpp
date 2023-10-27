d7e466e4a22f751eba9cd72c014d8844714d87a6
MojoisMe <2377862945@qq.com>
2023-10-27 15:10:26 +0800

    Complete Tree

--- qwq/Tree2.cpp
+++ qwq/Tree2.cpp
@@ -10,63 +10,76 @@ using namespace std;
 // }
 
 class Solution {
-    vector<int> idx_map;//idx:nodes中的下标, value:真实序号
-    vector<int> depth_map;//idx:nodes中的下标, value:深度
-    vector<int> nodes;
+    class TreeNode {
+    public:
+        int val, idx, depth;
+        TreeNode *l, *r, *f;
+        TreeNode(int _val = 0, int _idx = -1, int _depth = -1,
+            TreeNode *_l = nullptr, TreeNode *_r = nullptr, TreeNode *_f = nullptr) :
+            val(_val), idx(_idx), depth(_depth), l(_l), r(_r), f(_f) {
+            ;
+        }
+    }*root;
+    typedef TreeNode *Ptn;
+    // vector<int> idx_map; //idx:nodes中的下标, value:真实序号
+    // vector<int> depth_map; //idx:nodes中的下标, value:深度
+    // vector<int> nodes;
     int ansLength;
-    pair<int, int> anspairs;
+    pair<Ptn, Ptn> anspairs;
     int maxSum;
     int _size;
-    // idx为在nodes中的下标
-    int father(int idx) {
-        if (idx == 0) return -1;
-        return (idx - 1) >> 1;
-    }
-    int sonl(int idx) {
-        int s = (idx << 1) + 1;
-        if (s < _size) return s;
-        return -1;
-    }
-    int sonr(int idx) {
-        int s = (idx + 1) << 1;
-        if (s < _size) return s;
-        return -1;
+    void buildTree(vector<int> &nodes) {
+        queue<TreeNode *> curr_level, next_level;
+        int idx_depth = 1, idx_cnt = 0;
+        root = new TreeNode(nodes[0], idx_cnt++, 0);
+        curr_level.push(root);
+        for (int i = 1; ; ) {
+            TreeNode *curr_node = curr_level.front();
+            curr_level.pop();
+            if (nodes[i] != 0) {
+                auto sonl = new TreeNode(nodes[i], idx_cnt++, idx_depth, nullptr, nullptr, curr_node);
+                curr_node->l = sonl;
+                next_level.push(sonl);
+            }
+            if (++i >= nodes.size()) break;
+            if (nodes[i] != 0) {
+                auto sonr = new TreeNode(nodes[i], idx_cnt++, idx_depth, nullptr, nullptr, curr_node);
+                curr_node->r = sonr;
+                next_level.push(sonr);
+            }
+            if (++i >= nodes.size()) break;
+            if (curr_level.empty()) {
+                curr_level = next_level;
+                next_level = queue<TreeNode *>();
+                idx_depth++;
+            }
+        }
     }
 public:
     Solution(vector<int> &nodes) {
-        this->nodes = nodes;
+        buildTree(nodes);
         maxSum = -0x3f3f3f3f;
         ansLength = 0x3f3f3f3f;
-        anspairs.first = -1;
-        anspairs.second = -1;
-        _size = nodes.size();
-        idx_map.resize(_size, -1);
-        depth_map.resize(_size, -1);
-        int cnt = 0, depth = 0;
-
-        for (int i = 0; i < nodes.size(); ++i) {
-            if (nodes[i] != 0) idx_map[i] = cnt++;
-            depth_map[i] = depth;
-            if (i == (1 << (depth + 1)) - 2) ++depth;
-        }
+        anspairs.first = root;
+        anspairs.second = root;
     }
 
-    vector<int> findMiniMaxSumPath() {
-        helper(0);
+    vector<int> findMinLenthMaxSumPath() {
+        helper(root);
         vector<int> path;
         stack<int> edpath;
         // find the path
-        int st = anspairs.first, ed = anspairs.second;
-        if (idx_map[st] > idx_map[ed]) { swap(st, ed); }
-        while (depth_map[st] < depth_map[ed]) { edpath.push(idx_map[ed]); ed = father(ed); }
-        while (depth_map[st] > depth_map[ed]) { path.push_back(idx_map[st]); st = father(st); }
+        auto st = anspairs.first, ed = anspairs.second;
+        if (st->idx > ed->idx) { swap(st, ed); }
+        while (st->depth < ed->depth) { edpath.push(ed->idx); ed = ed->f; }
+        while (st->depth > ed->depth) { path.push_back(st->idx); st = st->f; }
         while (st != ed) {
-            path.push_back(idx_map[st]);
-            edpath.push(idx_map[ed]);
-            st = father(st);
-            ed = father(ed);
+            path.push_back(st->idx);
+            edpath.push(ed->idx);
+            st = st->f;
+            ed = ed->f;
         }
-        path.push_back(idx_map[st]);
+        path.push_back(st->idx);
         while (!edpath.empty()) {
             path.push_back(edpath.top()); edpath.pop();
         }
@@ -77,65 +90,73 @@ public:
 private:
 
     bool should_changeMaxi(int tempLength, int tempSum) {
-        if (tempSum > this->maxSum) return true;
-        else if (tempSum < this->maxSum) return false;
-        // else ==
-        if (tempLength <= this->ansLength) return true;
-        else if (tempLength > this->ansLength) return false;
+        return (tempSum > this->maxSum) || (tempSum == this->maxSum && tempLength <= ansLength);
+        // if (tempSum > this->maxSum) return true;
+        // else if (tempSum < this->maxSum) return false;
+        // // else ==
+        // if (tempLength <= this->ansLength) return true;
+        // else //(tempLength > this->ansLength)
+        //     return false;
         // 题目 说最大的只有一个 所以当tSum = mSum tLength = ansLength 时，\
         我们选择改变最大值，因为先出现的已经固定死了，后出现要么不是最大值，要么在后面会有更大的值加进来
-        assert(false);
+        // assert(false);
     }
 
-    int helper(int posidx) {
+    TreeNode *helper(TreeNode *node) {
         // 返回一个叶节点
-        if (posidx < 0 || nodes[posidx] == 0) return -1; // 空节点
-        int sl = sonl(posidx), sr = sonr(posidx);
+        if (node == nullptr) return nullptr; // 空节点
+        auto sl = node->l, sr = node->r;
         auto leafl = helper(sl);
         auto leafr = helper(sr);
-        int res = posidx;
-        int tempSum = nodes[posidx];
+        auto res = node;
+        int tempSum = node->val;
         int tempLength = 1;
-        pair<int, int> tempPair(posidx, posidx);
+        pair<Ptn, Ptn> tempPair(node, node);
 
-        if (sl == -1 && sr == -1); //没有儿子了
+        if (sl == nullptr && sr == nullptr); //没有儿子了
         // sl != -1 || sr != -1
-        else if (sl == -1 || nodes[sl] <= 0) // 左儿子没有或者没用
+        else if (sl == nullptr || sl->val <= 0) // 左儿子没有或者没用
         {
-            if (nodes[sr] <= 0); // 右儿子没用
-            else {
-                nodes[posidx] += nodes[sr];
-                tempSum = nodes[posidx];
+            if (sr == nullptr || sr->val <= 0); // 右儿子没有或者没用
+            else { // 右儿子有且有用
+                node->val += sr->val;
+                tempSum = node->val;
                 tempPair.first = leafr;
-                tempPair.second = posidx;
-                tempLength = depth_map[leafr] - depth_map[posidx] + 1;
+                tempPair.second = node;
+                tempLength = leafr->depth - node->depth + 1;
                 res = leafr;
             }
         }
         // sl != -1 && nodes[sl] > 0
-        else if (sr == -1 || nodes[sr] <= 0)// 右儿子没有或者没用
+        else if (sr == nullptr || sr->val <= 0)// 右儿子没有或者没用
         {
-            if (nodes[sl] <= 0); // 左儿子没用 不会发生
-            else {
-                nodes[posidx] += nodes[sl];
-                tempSum = nodes[posidx];
+            if (sl == nullptr || sl->val <= 0); // 左儿子没有或者没用
+            else { // 左儿子有且有用
+                node->val += sl->val;
+                tempSum = node->val;
                 tempPair.first = leafl;
-                tempPair.second = posidx;
-                tempLength = depth_map[leafl] - depth_map[posidx] + 1;
+                tempPair.second = node;
+                tempLength = leafl->depth - node->depth + 1;
                 res = leafl;
             }
         }
         // sl != -1 && nodes[sl] > 0 && sr != -1 && nodes[sr] > 0
-        else {
-            tempSum = nodes[posidx] + nodes[sl] + nodes[sr];
+        else if (sl && sl->val > 0 && sr && sr->val > 0) {
+            tempSum = node->val + sl->val + sr->val;
             tempPair.first = leafl;
             tempPair.second = leafr;
-            tempLength = depth_map[leafl] + depth_map[leafr] - 2 * depth_map[posidx] + 1;
-            res = nodes[sl] > nodes[sr] ? leafl : (nodes[sl] < nodes[sr] ? leafr : (depth_map[leafl] < depth_map[leafr] ? leafl : leafr));// 不会出现 ==
-            nodes[posidx] += (res == leafl ? nodes[sl] : nodes[sr]);
+            tempLength = leafl->depth + leafr->depth - 2 * node->depth + 1;
+            bool choseL = true;
+            if (sl->val < sr->val) choseL = false;
+            else if (sl->val == sr->val) {
+                if (leafl->depth > leafr->depth) choseL = false;
+                // == 无意义
+            }
+            res = choseL ? leafl : leafr;
+            node->val += choseL ? sl->val : sr->val;
         }
         // _output << tempSum << " ";
-        if (should_changeMaxi(tempLength,tempSum)) {
+        if (should_changeMaxi(tempLength, tempSum)) {
             // _output << "changed ";
             this->maxSum = tempSum;
             this->ansLength = tempLength;
@@ -157,7 +178,7 @@ int main() {
         array.push_back(num);
     }
     Solution solution(array);
-    vector<int> path = solution.findMiniMaxSumPath();
+    vector<int> path = solution.findMinLenthMaxSumPath();
     for (auto &n : path) {
         cout << n << " ";
     }
---
+++ qwq/Tree3.cpp
@@ -0,0 +1,233 @@
+#include<iostream>
+#include<queue>
+#include<vector>
+
+using namespace std;
+
+struct TreeNode {
+    int val;//值
+    int seq;//顺序
+    int left_tree_max_sum;//左子树最大路径和
+    int right_tree_max_sum;//右子树最大路径和
+    int total_tree_max_sum;//以该节点为根的树的最大路径和
+    int path_left_len;//最大路径落在左子树的长度
+    int path_right_len;//最大路径落在右子树的长度
+    int path_total_len;//最大路径长度
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode *parent;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(TreeNode &treenode) :val(treenode.val), seq(treenode.seq), left(treenode.left), right(treenode.right), parent(treenode.parent) {
+        left_tree_max_sum = treenode.left_tree_max_sum;
+        right_tree_max_sum = treenode.right_tree_max_sum;
+        total_tree_max_sum = treenode.total_tree_max_sum;
+        path_left_len = treenode.path_left_len;
+        path_right_len = treenode.path_right_len;
+        path_total_len = treenode.path_total_len;
+    }
+};
+
+int cnt = 0;//用于生成节点编号
+int max_length = INT_MIN;
+int max_single = INT_MIN;
+vector<vector<int>> res;//求和等于最大值的 有用的路径(但还没有最短)
+
+TreeNode *buildTree(int value[], int sz) {//将层序遍历还原成二叉树
+    if (sz == 0) return nullptr;
+    TreeNode *root = new TreeNode(value[0]);
+    root->seq = cnt++;
+    queue<TreeNode *> queue;
+    queue.emplace(root);
+
+    for (int i = 1; i < sz; i++) {
+        TreeNode *front = queue.front();
+        queue.pop();
+        if (value[i] != 0) {
+            TreeNode *left = new TreeNode(value[i]);
+            left->parent = front;
+            left->seq = cnt++;
+            front->left = left;
+            queue.emplace(left);
+        }
+        if (value[++i] != 0) {
+            TreeNode *right = new TreeNode(value[i]);
+            right->parent = front;
+            right->seq = cnt++;
+            front->right = right;
+            queue.emplace(right);
+        }
+    }
+    return root;
+}
+
+int max_sum_of_tree(TreeNode *root, int &max_length) {//寻找最大路径和，并且更新每个节点的求和信息
+    if (root == nullptr) return 0;
+    root->left_tree_max_sum = max_sum_of_tree(root->left, max_length);
+    root->right_tree_max_sum = max_sum_of_tree(root->right, max_length);
+    root->total_tree_max_sum = max(root->left_tree_max_sum + root->right_tree_max_sum + root->val, 0);
+    max_length = max_length < root->total_tree_max_sum ? root->total_tree_max_sum : max_length;
+    return max(0, max(root->left_tree_max_sum, root->right_tree_max_sum) + root->val);
+}
+
+int generate_len(TreeNode *root) {//更新每个节点的最长路径长以及该路径落在左边和右边的长
+    if (root == nullptr) return 0;
+    root->path_left_len = generate_len(root->left);
+    root->path_right_len = generate_len(root->right);
+    root->path_total_len = root->path_left_len + root->path_right_len + 1;
+    // if (root->total_tree_max_sum == max_length) seperate_path(root);//递归还没完成，此时就提取路径会有问题
+    if (root->left_tree_max_sum + root->val <= 0 && root->right_tree_max_sum + root->val <= 0) return 0;
+
+    if (root->left_tree_max_sum > root->right_tree_max_sum) return root->path_left_len + 1;
+    else if (root->left_tree_max_sum < root->right_tree_max_sum) return root->path_right_len + 1;
+    else return min(root->path_left_len, root->path_right_len) + 1;//"V"形结构不能递归，只能如此return
+}
+
+void seperate_path(TreeNode *root, vector<int> &path, int seq) {//寻找路径和等于最大值的部分有用的路径
+    if (root == nullptr) return;
+    //策略:默认从左到中间再到右边,后面输出的时候需要的话再调换
+    if (root->path_left_len != 0 && root->path_right_len != 0 && root->seq == seq)//头节点:左边递归输出，输出root，右边顺序输出
+    {
+        seperate_path(root->left, path, seq);//左边
+        path.push_back(root->seq);//root
+        if (root->path_right_len != 0)//右边
+        {
+            TreeNode *head(root);
+            head = head->right;
+            path.push_back(head->seq);
+            while (head->path_right_len != 0 || head->path_left_len != 0)//能走下去
+            {
+                if (head->path_left_len != 0 && head->path_right_len != 0)//有分叉
+                {
+                    //优先选择值大的
+                    if (head->left_tree_max_sum > head->right_tree_max_sum) {
+                        head = head->left;
+                        path.push_back(head->seq);
+                    }
+                    else if (head->left_tree_max_sum < head->right_tree_max_sum) {
+                        head = head->right;
+                        path.push_back(head->seq);
+                    }
+                    //值相等选择路径短的
+                    else {
+                        if (head->path_left_len <= head->path_right_len) {
+                            head = head->left;
+                            path.push_back(head->seq);
+                        }
+                        else {
+                            head = head->right;
+                            path.push_back(head->seq);
+                        }
+                    }
+                }
+                //无分叉
+                else {
+                    if (head->path_left_len != 0) {
+                        head = head->left;
+                        path.push_back(head->seq);
+                    }
+                    else if (head->path_right_len != 0) {
+                        head = head->right;
+                        path.push_back(head->seq);
+                    }
+                }
+            }
+        }
+    }
+    else//非根节点，递归下去即可,同理
+    {
+        if (root->path_left_len != 0 && root->path_right_len != 0) {
+            if (root->left_tree_max_sum > root->right_tree_max_sum) {
+                seperate_path(root->left, path, seq);
+            }
+            else if (root->left_tree_max_sum < root->right_tree_max_sum) {
+                seperate_path(root->right, path, seq);
+            }
+            else {
+                if (root->path_left_len <= root->path_right_len) {
+                    seperate_path(root->left, path, seq);
+                }
+                else {
+                    seperate_path(root->right, path, seq);
+                }
+            }
+        }
+        else {
+            if (root->path_left_len != 0) seperate_path(root->left, path, seq);
+            else if (root->path_right_len != 0) seperate_path(root->right, path, seq);
+        }
+        path.push_back(root->seq);
+    }
+}
+
+void preorder(TreeNode *root) {//前序遍历每个根节点的最大路径,寻找可能的答案
+    if (root == nullptr) return;
+    if (root->total_tree_max_sum == max_length)//找到一条求和符合要求的
+    {
+        vector<int>path;
+        seperate_path(root, path, root->seq);
+        res.push_back(path);
+    }
+    preorder(root->left);
+    preorder(root->right);
+}
+
+void preorder_all_minus_case(TreeNode *root) {//处理节点数值全是负数的特殊情况
+    if (root == nullptr) return;
+    if (root->val == max_single) {
+        cout << root->seq;
+    }
+    preorder_all_minus_case(root->left);
+    preorder_all_minus_case(root->right);
+}
+
+int main(void) {
+    int n;
+    cin >> n;
+    int v[60005] = { 0 };
+    bool has_positive = false;//是否有正数
+    for (int i = 0;i < n;i++) {
+        cin >> v[i];
+        if (v[i] >= 0) has_positive = true;
+    }
+
+    TreeNode *head = buildTree(v, n + 1);//多一位防止树构建出问题
+
+    if (!has_positive)//没正数的特殊情况
+    {
+        if (has_positive == false) {
+            max_single = v[0];
+            for (int i = 0;i < n;i++) {
+                max_single = max_single < v[i] ? v[i] : max_single;
+            }
+        }
+        preorder_all_minus_case(head);
+        return 0;
+    }
+
+    max_sum_of_tree(head, max_length);//求路径和
+    generate_len(head);//生成路径长度数值和分布信息
+    preorder(head);//前序遍历寻找路径
+
+    //寻找符合要求的最短的路径,必要时反向打印
+    int min = res[0].size();
+    int minloc = 0;
+
+    for (int i = 0;i < res.size();i++) {
+        if (min > res[i].size()) {
+            min = res[i].size();
+            minloc = i;
+        }
+    }
+    if (res[minloc][0] < res[minloc][res[minloc].size() - 1]) {
+        for (int i = 0;i < res[minloc].size();i++) {
+            cout << res[minloc][i] << " ";
+        }
+    }
+    else {
+        for (int i = res[minloc].size() - 1;i >= 0;i--) {
+            cout << res[minloc][i] << " ";
+        }
+    }
+    return 0;
+}
