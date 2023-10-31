f0b67419f62209b3db1f831152ca13193cc4831b
MojoisMe <2377862945@qq.com>
2023-10-31 11:43:35 +0800

    每日一题未完成

---
+++ leetcode/LC_2003_lackedGenes.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    stack<int> genes;
+    vector<int> smallestMissingValueSubtree(vector<int> &parents, vector<int> &nums) {
+        vector<int> ans(parents.size(), -1);
+
+    }
+};
+
+int main() {
+    
+    //system("pause");
+    return 0;
+}
---
+++ leetcode/LC_2003_lackedGenes.py
@@ -0,0 +1,5 @@
+from typing import List
+class Solution:
+    def smallestMissingValueSubtree(self, parents: List[int], nums: List[int]) -> List[int]:
+        length = len(parents)
+        return []
