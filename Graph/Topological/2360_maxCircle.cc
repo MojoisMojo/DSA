c01920d2d2e6f2003b0c6116d9fff5fa1808ee19
MojoisMe <2377862945@qq.com>
2023-11-23 16:51:43 +0800

    dsa

---
+++ Graph/Topological/2360_maxCircle.cc
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+    vector<int> ans;
+public:
+    int longestCycle(vector<int>& edges) {
+        
+    }
+};
+
+int main() {
+    
+    //system("pause");
+    return 0;
+}
