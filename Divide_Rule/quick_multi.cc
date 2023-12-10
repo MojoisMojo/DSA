31b9f28d6790a2aa680c9c097538e372ef2927cb
MojoisMe <2377862945@qq.com>
2023-12-10 13:36:12 +0800

    Changes to be committed: 	modified:   Divide_Rule/quick_multi.cc

--- Divide_Rule/quick_multi.cc
+++ Divide_Rule/quick_multi.cc
@@ -4,7 +4,7 @@
 /// @param exp must be positive 
 /// @param mod 
 /// @return 
-long long pow(long long base, int exp, int mod) {
+long long pow(long long base, int exp, long long mod = INT64_MAX) {
     long long res = 1;
     for (; exp; exp /= 2) {
         if (exp % 2) res = res * base % mod;
