fbc767a9f0c754cf7adb09233fa6d227ce538511
MojoisMe <2377862945@qq.com>
2023-12-14 19:05:52 +0800

    deleted:    DP/Leetcode/tempCodeRunnerFile.py ...

--- DP/Leetcode/tempCodeRunnerFile.py
+++
@@ -1,8 +0,0 @@
-    def canbeSumToTarget(self,array:List[int],target:int)->bool:
-        n = len(array)
-        dp = [False for _ in range(target + 1)]
-        dp[0] = True
-        for i in range(n-1,-1,-1):
-            for t in range(target,array[i]-1,-1):
-                dp[t] |= dp[t - array[i]]
-        return dp[target]
